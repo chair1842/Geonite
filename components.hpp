b0aa65e14833392dd583ecedfde862660a634dff
chair1842 <207965457+chair1842@users.noreply.github.com>
2025-10-28 13:42:58 +0800

    add components.hpp

---
+++ components.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Position { float x; float y; };
+struct Velocity { float x; float y; };
+struct Health { int hp; };
+struct Input { bool up; bool down; bool left; bool right; bool confirm; };;
+struct PlayerTag {};
+struct NPCTag {};
