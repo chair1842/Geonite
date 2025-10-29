8e63c1cba07ff5e477b4da08cd62573de5204773
chair1842 <207965457+chair1842@users.noreply.github.com>
2025-10-29 11:08:21 +0800

    combine repeated cases and fix braces

--- player_systems.cpp
+++ player_systems.cpp
@@ -1,104 +1,81 @@
 #include "systems.hpp"
 
 void PlayerInputSystem(ecs::Registry& registry, SDL_Event& event) {
-	registry.view<PlayerTag, Input>([&event](PlayerTag&, Input& input) {
-		switch (event.type) {
-		case SDL_KEYDOWN:
-			switch (event.key.keysym.sym) {
-			case SDLK_w:
-				input.up = true;
-				break;
-			case SDLK_UP:
-				input.up = true;
-				break;
-			case SDLK_s:
-				input.down = true;
-				break;
-			case SDLK_DOWN:
-				input.down = true;
-				break;
-			case SDLK_a:
-				input.left = true;
-				break;
-			case SDLK_LEFT:
-				input.left = true;
-				break;
-			case SDLK_d:
-				input.right = true;
-				break;
-			case SDLK_RIGHT:
-				input.right = true;
-				break;
-			case SDLK_ESCAPE:
-				input.quit = true;
-				break;
-			case SDLK_RETURN:
-				input.action = true;
-				break;
-			case SDLK_e:
-				input.action = true;
-				break;
-			case SDLK_SPACE:
-				input.action = true;
-				break;
-			case SDLK_j:
-				input.attack = true;
-				break;
-			case SDLK_LSHIFT:
-				input.sprint = true;
-				break;
-			default:
-				break;
-			}
-			break;
-		case SDL_KEYUP:
-			switch (event.key.keysym.sym) {
-			case SDLK_w:
-				input.up = false;
-				break;
-			case SDLK_UP:
-				input.up = false;
-				break;
-			case SDLK_s:
-				input.down = false;
-				break;
-			case SDLK_DOWN:
-				input.down = false;
-				break;
-			case SDLK_a:
-				input.left = false;
-				break;
-			case SDLK_LEFT:
-				input.left = false;
-				break;
-			case SDLK_d:
-				input.right = false;
-				break;
-			case SDLK_RIGHT:
-				input.right = false;
-				break;
-			case SDLK_ESCAPE:
-				input.quit = false;
-				break;
-			case SDLK_RETURN:
-				input.action = false;
-				break;
-			case SDLK_e:
-				input.action = false;
-				break;
-			case SDLK_SPACE:
-				input.action = false;
-				break;
-			case SDLK_j:
-				input.attack = false;
-				break;
-			case SDLK_LSHIFT:
-				input.sprint = false;
-				break;
-			default:
-				break;
-			}
-			break;
-		});
-	}
+    registry.view<PlayerTag, Input>([&event](PlayerTag&, Input& input) {
+        switch (event.type) {
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_w:
+            case SDLK_UP:
+                input.up = true;
+                break;
+            case SDLK_s:
+            case SDLK_DOWN:
+                input.down = true;
+                break;
+            case SDLK_a:
+            case SDLK_LEFT:
+                input.left = true;
+                break;
+            case SDLK_d:
+            case SDLK_RIGHT:
+                input.right = true;
+                break;
+            case SDLK_ESCAPE:
+                input.quit = true;
+                break;
+            case SDLK_RETURN:
+            case SDLK_e:
+            case SDLK_SPACE:
+                input.action = true;
+                break;
+            case SDLK_j:
+                input.attack = true;
+                break;
+            case SDLK_LSHIFT:
+                input.sprint = true;
+                break;
+            default:
+                break;
+            }
+            break;
+
+        case SDL_KEYUP:
+            switch (event.key.keysym.sym) {
+            case SDLK_w:
+            case SDLK_UP:
+                input.up = false;
+                break;
+            case SDLK_s:
+            case SDLK_DOWN:
+                input.down = false;
+                break;
+            case SDLK_a:
+            case SDLK_LEFT:
+                input.left = false;
+                break;
+            case SDLK_d:
+            case SDLK_RIGHT:
+                input.right = false;
+                break;
+            case SDLK_ESCAPE:
+                input.quit = false;
+                break;
+            case SDLK_RETURN:
+            case SDLK_e:
+            case SDLK_SPACE:
+                input.action = false;
+                break;
+            case SDLK_j:
+                input.attack = false;
+                break;
+            case SDLK_LSHIFT:
+                input.sprint = false;
+                break;
+            default:
+                break;
+            }
+            break;
+        }
+        });
 }
